9d73743e71e82eaa4d0a1fe7480c5b4c94f6e5ec
alexisfontaine <alexis.fontaine@isep.fr>
2015-01-18 17:01:19 +0100

    Fix lien espace_membre

--- global/espace_membre.php
+++ global/espace_membre.php
@@ -10,8 +10,8 @@
                 <?php else : ?>
                 <ul>
                     <img class="flottant_droite" src="<?= $_SESSION['Utilisateur']['avatar']; ?>" title="Avatar de <?= htmlspecialchars($_SESSION['Utilisateur']['avatar']); ?>">
-                    <li><a href="<?= url('membres', 'afficher'); ?>'id' => $_SESSION['Utilisateur']['id']; ?>">Mon profil</a></li>
-                    <li><a href="<?= url('logements', 'lister'); ?>'id' => $_SESSION['Utilisateur']['id']; ?>">Mes logements</a></li>
+                    <li><a href="<?= url('membres', 'afficher', ['id' => $_SESSION['Utilisateur']['id']]); ?>">Mon profil</a></li>
+                    <li><a href="<?= url('logements', 'lister', ['id' => $_SESSION['Utilisateur']['id']]); ?>">Mes logements</a></li>
                     <li><a href="<?= url('disponibilites', 'afficher_reservations'); ?>">Mes réservations</a></li>
                     <li><a href="<?= url('logements', 'inscription'); ?>">Proposer un logement</a></li>
                     <li><a href="<?= url('membres', 'modifier_profil'); ?>">Modifier mon profil</a></li>
