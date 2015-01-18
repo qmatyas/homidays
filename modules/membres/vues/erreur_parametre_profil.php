c50855e589064c0d5eaa0a35de56713a9dd67e0e
dadooooo <romaince@hotmail.fr>
2015-01-18 16:43:49 +0100

    nouveautés

--- modules/membres/vues/erreur_parametre_profil.php
+++ modules/membres/vues/erreur_parametre_profil.php
@@ -1,2 +1,3 @@
 <h2>Erreur d'affichage du profil</h2>
+
 <p>Veuillez précisez l'id de l'utilisateur dont vous voulez voir le profil.</p>
--- modules/membres/vues/formulaire_connexion.php
+++ modules/membres/vues/formulaire_connexion.php
@@ -1,18 +1,17 @@
 <h2>Connexion au site</h2>
 
-<p>Si vous n'êtes pas encore inscrit, vous pouvez le faire en <a href="index.php?module=membres&amp;action=inscription">cliquant sur ce lien</a>.</p>
-
 <form action="" method="post">
 	<?= isset($erreur['form']) ? $erreur['form'] : ''; ?>
 	<p>		
-		<label for="pseudo">Pseudo :</label>
+		<label for="pseudo"><b>Pseudo :</b></label>
 		<input type="text" name="pseudo" id="pseudo">
 		<?= isset($erreur['pseudo']) ? $erreur['pseudo'] : ''; ?>
-	</p><p>
-		<label for="pass">Mot de passe:</label>
+	</p>
+	<p>
+		<label for="pass"><b>Mot de passe :</b></label>
 		<input type="password" name="pass" id="pass">
 		<?= isset($erreur['pass']) ? $erreur['pass'] : ''; ?>
 	<p/>
 	<input type="submit" value="Se connecter" onClick="criteres();">			
 </form>
-<p><a href="#"> Mot de passe oublié ?</a></p>
+<!-- <p><a href="#"> Mot de passe oublié ?</a></p> -->	
