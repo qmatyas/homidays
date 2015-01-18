48ffb22fe324ac0dcf59a26216b50f659425cf12
dadooooo <romaince@hotmail.fr>
2015-01-18 15:58:29 +0100

    ACCUEIL

--- global/accueil.php
+++ global/accueil.php
@@ -1,8 +1,6 @@
 <h1>Bienvenue à la maison</h1>
 <h2>Échangez vos maisons gratuitement entre particuliers du monde entier !</h2>
 
-<h2>Recherche</h2>
-
 <form action="index.php?module=logements&action=lister" method="get" class="recherche">
     <input type="hidden" name="module" value="logements">
     <input type="hidden" name="action" value="lister">
